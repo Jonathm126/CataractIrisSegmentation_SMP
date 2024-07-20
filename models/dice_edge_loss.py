bd046ad614890fb03597bb8848f804b13f49f9ba
Jonathm126 <feedmeh@gmail.com>
2024-07-20 19:02:43 +0300

    added graphs and things for the final report

--- models/dice_edge_loss.py
+++ models/dice_edge_loss.py
@@ -16,15 +16,24 @@ def extract_volume_2D_contours_pt(mask, N, th=1):
 
 def contour_dice_loss(y_true, y_pred, gamma=1, th = 1):
     # dice function
-    dice = DiceLoss(BINARY_MODE)
+    dice_logits = DiceLoss(BINARY_MODE)
+    dice_mask = DiceLoss(BINARY_MODE, from_logits=False)
     
     # compute loss
-    dice_loss = dice(y_true, y_pred)
+    dice_loss = dice_logits(y_true, y_pred)
     
     # compute contour loss
     N = y_true.shape[0]
+    
+    # convnert from logits to int
+    y_true = y_true.sigmoid()
+    y_pred = y_pred.sigmoid()
+    
+    # compute contours
     true_contours = extract_volume_2D_contours_pt(y_true[:, :, :, :], N, th)
     pred_contours = extract_volume_2D_contours_pt(y_pred[:, :, :, :], N, th)
-    contour_dice_loss = dice(true_contours, pred_contours)
+    
+    # compute dice from masks (not logits)
+    contour_dice_loss = dice_mask(true_contours, pred_contours)
     
     return dice_loss + (gamma * contour_dice_loss)
